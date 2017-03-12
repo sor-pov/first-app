3518378237b61aba1f3b3b87d4df933a975d078b
thong-trainer <thong.education@gmail.com>
2017-03-12 18:53:35 +0700

    THong: added

---
+++ controller/EmployeeController.php
@@ -0,0 +1,14 @@
+<?PHP
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$database_name = 'first_db';
+$con = mysqli_connect($server_name, $user_name, $password, $database_name);
+
+if(!$con){
+	die("Error connection");
+}else{
+	echo "Successfully";
+}
+
+?>
